dafd9bac86686542fe434d5672672e1f12bc7d6a
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-02-04 20:14:11 +0800

    Update fireplace.hx

--- content/W1_CHRISTMAS/stages/fireplace.hx
+++ content/W1_CHRISTMAS/stages/fireplace.hx
@@ -13,12 +13,6 @@ function onCreatePost()
     game.snapCamFollowToPos(bg.x + (bg.width / 2), bg.y + (bg.height / 2));
     modManager.setValue("transformX", 75, 0);
     modManager.setValue("transformX", -75, 1);
-    
-    if (ClientPrefs.shadersEnabled) {
-        var vhs:FlxShader = newShader('vhs');
-        var filter:ShaderFilter = new ShaderFilter(vhs); // I'll rewrite this slightly to make it to use an for i loop
-        for(c in [game.camGame, game.camHUD, game.camOther]) c.setFilters([filter]);  
-    }
 
     FlxG.scaleMode.width = 960;    
     FlxG.camera.width = 960;
